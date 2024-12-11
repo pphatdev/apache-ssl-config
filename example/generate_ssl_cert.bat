a741332134b5a99515550ac34417c3b0f9168b3f
PPhat DEv <leatsophat02@gmail.com>
2024-12-11 15:37:59 +0700

    ⚙️: Create generate certificate command

---
+++ example/generate_ssl_cert.bat
@@ -0,0 +1,1 @@
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout C:/xampp/apache/conf/ssl.key/pphatdev.local.key -out C:/xampp/apache/conf/ssl.crt/pphatdev.local.crt -config pphatdev.local.cnf
